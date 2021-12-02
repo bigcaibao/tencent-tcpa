2c185611be33110f3e5398968111e758ad9df8b5
CaiBao <31830245+bigcaibao@users.noreply.github.com>
2021-12-03 00:09:54 +0800

    Update tcpa.sh

--- tcpa.sh
+++ tcpa.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#喜欢记得收藏一下 www.caibao.in
+#喜欢记得收藏一下 Mrx.la
 PATH=$PATH:/sbin:/usr/sbin:/usr/local/sbin:$HOME/bin:/usr/local/bin:/usr/bin
 export PATH
 export LANG=en_US.UTF-8
@@ -7,7 +7,7 @@ export LANG=en_US.UTF-8
 yum -y install epel-release
 yum -y install net-tools
 
-curl -o /tmp/tcpa_packets_180619_1151.tar.gz http://down.caibao.in/Git/TCPA/tcpa_packets_180619_1151.tar.gz
+curl -o /tmp/tcpa_packets_180619_1151.tar.gz https://github.com/bigcaibao/tencent-tcpa/tcpa_packets_180619_1151.tar.gz
 chmod +x /etc/rc.d/rc.local
 cat>>/etc/rc.local<<EOF
 ####tcpa####
@@ -24,7 +24,7 @@ sed -i '/####tcpa####/','/####tcpa####/d' /etc/rc.local
 ####tcpa####
 EOF
 
-rpm -ivh http://down.caibao.in/Git/TCPA/kernel-3.10.0-693.5.2.tcpa06.tl2.x86_64.rpm --force
+rpm -ivh https://github.com/bigcaibao/tencent-tcpa/releases/download/1.0/kernel-3.10.0-693.5.2.tcpa06.tl2.x86_64.rpm --force
 echo "内核安装完毕,3秒后将自动重启..."
 echo "重启后安装自动完成,lsmod|grep tcpa查看是否开启成功."
 sleep 3
